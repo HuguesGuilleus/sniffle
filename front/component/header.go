9d5d62b9b9af8ec0453f00c29c0e00506ee66fc7
Hugues GUILLEUS <ghugues@netc.fr>
2024-08-09 20:18:38 +0200

    Add front/component/ Header() & use it

--- front/component/header.go
+++ front/component/header.go
@@ -21,6 +21,28 @@ func InDevHeader(l language.Language) render.Node {
 	return render.N("div.subHeader", translate.AllTranslation[l].InDev)
 }
 
+func Header(langs []language.Language, pageLang language.Language, idNamespace, idCode render.Node, title string) []render.Node {
+	return []render.Node{
+		render.N("header",
+			render.N("div.headerSup", idNamespace, idCode),
+			render.N("div.headerTitle", title),
+			render.N("div.headerLangs", render.Slice(langs, func(_ int, l language.Language) render.Node {
+				if pageLang == l {
+					return render.No("span.headerOneLang",
+						render.A("title", l.Human()),
+						l.String())
+				}
+				return render.No("a.headerOneLang",
+					render.
+						A("title", l.Human()).
+						A("hreflang", l.String()).
+						A("href", l.String()+".html"),
+					l.String())
+			})),
+		),
+	}
+}
+
 // A footer node. It should be the last element in the page.
 // It contain in the end, so the DOM is complete when it's executed.
 func Footer(l language.Language) render.Node {
--- service/about/about.go
+++ service/about/about.go
@@ -21,22 +21,17 @@ func Do(_ context.Context, t *tool.Tool) {
 			Title:       tr.ABOUT.PageTitle,
 			Description: tr.ABOUT.PageDescription,
 			BaseURL:     basePath,
-		}
-		page.Body = render.N("body.edito",
-			component.TopHeader(l),
-			render.N("header",
-				render.N("div.headerSup",
-					render.N("div.headerId",
-						render.No("a",
-							render.A("href", "/").A("title", tr.HomeTitle),
-							tr.HomeName),
-					),
-				),
-				render.N("div.headerTitle", tr.ABOUT.PageTitle),
+			Body: render.N("body.edito",
+				component.TopHeader(l),
+				component.Header(t.Languages, l, render.N("div.headerId",
+					render.No("a",
+						render.A("href", "/").A("title", tr.HomeTitle),
+						tr.HomeName),
+				), render.Z, tr.ABOUT.PageTitle),
+				render.N("div.w", tr.ABOUT.Text),
+				component.Footer(l),
 			),
-			render.N("div.w", tr.ABOUT.Text),
-			component.Footer(l),
-		)
+		}
 
 		component.Html(t, &page)
 	}
