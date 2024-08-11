82667ec4004cf2a3eef5e7592c2d882eb7f9d992
Hugues GUILLEUS <ghugues@netc.fr>
2024-08-11 17:18:54 +0200

    Add service/eu_ec_eci/ unknown warning

--- service/eu_ec_eci/service.go
+++ service/eu_ec_eci/service.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"sniffle/front/component"
+	"sniffle/front/translate"
 	"sniffle/tool"
+	"sniffle/tool/language"
 	"sniffle/tool/render"
 )
 
@@ -25,6 +27,16 @@ func Do(ctx context.Context, t *tool.Tool) {
 		}
 		if eci.ImageName != "" {
 			t.WriteFile(fmt.Sprintf("/eu/ec/eci/%d/%d/%s", eci.Year, eci.Number, eci.ImageName), eci.ImageData)
+		}
+
+		// Warn about unknown categorie and status
+		for _, categorie := range eci.Categorie {
+			if translate.AllTranslation[language.English].EU_EC_ECI.Categorie[categorie] == "" {
+				t.Warn("unknown.categorie", "categorie", categorie)
+			}
+		}
+		if translate.AllTranslation[language.English].EU_EC_ECI.Status[eci.Status] == "" {
+			t.Warn("unknown.status", "status", eci.Status)
 		}
 	}
 
