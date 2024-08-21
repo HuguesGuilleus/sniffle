41dda9d15b1d956e3ff8e06ca07d63245bf7161b
Hugues GUILLEUS <ghugues@netc.fr>
2024-08-21 11:33:46 +0200

    Add main.go cli flag

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log/slog"
 	"os"
 	"sniffle/myhandler"
@@ -14,20 +15,29 @@ import (
 )
 
 func main() {
-	logger := slog.New(myhandler.New(os.Stdout, tool.NoticeLevel))
-	// logger = slog.New(myhandler.New(os.Stdout, slog.LevelInfo))
-	// logger = slog.New(myhandler.New(os.Stdout, slog.LevelDebug))
+	level := levelValue{Level: tool.NoticeLevel}
+	flag.Var(&level, "log", "The log level: DEBUG|INFO|WARN|ERROR (case insensitive, suport int offset)")
+	out := flag.String("out", "public", "The output directory")
+	cache := flag.String("cache", "cache", "The cache directory")
+	host := flag.String("host", "https://sniffle.eu/", "The host absolute URL")
+	flag.Parse()
 
 	config := tool.Config{
-		Logger:    logger,
-		HostURL:   "https://sniffle.eu/",
+		Logger:    slog.New(myhandler.New(os.Stdout, level.Level)),
+		HostURL:   *host,
 		Languages: []language.Language{language.English, language.French},
-		Writefile: writefile.Os("public"),
+		Writefile: writefile.Os(*out),
 		Fetcher: []fetch.Fetcher{
-			fetch.CacheOnly("cache"),
-			fetch.Net(nil, "cache", 1, time.Millisecond*100),
+			fetch.CacheOnly(*cache),
+			fetch.Net(nil, *cache, 1, time.Millisecond*100),
 		},
 	}
 
 	tool.Run(context.Background(), &config, service.List)
 }
+
+type levelValue struct {
+	slog.Level
+}
+
+func (l *levelValue) Set(s string) error { return l.Level.UnmarshalText([]byte(s)) }
