3c1bbb4000018a010feb4ff79b60670c03b271bd
Hugues GUILLEUS <ghugues@netc.fr>
2024-08-27 20:33:59 +0200

    Add main.go logOUt and logJson flag

--- main.go
+++ main.go
@@ -17,14 +17,31 @@ import (
 func main() {
 	level := levelValue{Level: tool.NoticeLevel}
 	flag.Var(&level, "log", "The log level: DEBUG|INFO|WARN|ERROR (case insensitive, suport int offset)")
+	logOut := flag.String("logout", "-", "The output file to append logs")
+	logJson := flag.Bool("logjson", false, "Use json handler ")
 	out := flag.String("out", "public", "The output directory")
 	cache := flag.String("cache", "cache", "The cache directory")
 	host := flag.String("host", "https://sniffle.eu/", "The host absolute URL")
 	dev := flag.Bool("dev", false, "Dev mode")
 	flag.Parse()
 
+	logFile := os.Stderr
+	if *logOut != "-" {
+		err := error(nil)
+		logFile, err = os.OpenFile(*logOut, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o664)
+		if err != nil {
+			os.Stderr.WriteString(err.Error())
+			os.Exit(1)
+		}
+	}
+
+	logHandler := myhandler.New(logFile, level.Level)
+	if *logJson {
+		logHandler = slog.NewJSONHandler(logFile, &slog.HandlerOptions{Level: level.Level})
+	}
+
 	config := tool.Config{
-		Logger:    slog.New(myhandler.New(os.Stdout, level.Level)),
+		Logger:    slog.New(logHandler),
 		HostURL:   *host,
 		Languages: []language.Language{language.English, language.French},
 		Writefile: writefile.Os(*out),
