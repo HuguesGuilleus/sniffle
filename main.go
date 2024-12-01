7403c38547abc6909a8107f0f8668bdc0474e619
Hugues GUILLEUS <ghugues@netc.fr>
2024-12-01 01:34:08 +0100

    Merge service/index.go -> main.go

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"flag"
-	"sniffle/service"
+	"sniffle/common/resize0"
+	"sniffle/front"
+	"sniffle/service/about"
+	"sniffle/service/eu_ec_eci"
+	"sniffle/service/eu_eca"
+	"sniffle/service/home"
 	"sniffle/tool"
 	"sniffle/tool/language"
+	"sniffle/tool/render"
 	"time"
 )
 
@@ -15,7 +21,28 @@ func main() {
 
 	config.HostURL = *host
 	config.Languages = []language.Language{language.English, language.French}
-	config.LongTasksMap = service.LongTask
 
-	tool.Run(config, service.List...)
+	config.LongTasksMap = map[string]func([]byte) ([]byte, error){
+		resize0.Name: resize0.Resize,
+	}
+
+	tool.Run(config,
+		tool.Service{Name: "front", Do: front.Do},
+		tool.Service{Name: "notImplementedPage", Do: notImplementedPage},
+
+		tool.Service{Name: "about", Do: about.Do},
+		tool.Service{Name: "home", Do: home.Do},
+
+		tool.Service{Name: "eu_ec_eci", Do: eu_ec_eci.Do},
+		tool.Service{Name: "eu_eca", Do: eu_eca.Do},
+	)
+}
+
+func notImplementedPage(t *tool.Tool) {
+	for _, l := range t.Languages {
+		t.WriteFile("/eu/index.html", render.Back)
+		t.WriteFile("/eu/"+l.String()+".html", render.Back)
+		t.WriteFile("/eu/ec/index.html", render.Back)
+		t.WriteFile("/eu/ec/"+l.String()+".html", render.Back)
+	}
 }
