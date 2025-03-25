d94d31252b22fde8ec6590841316a2a55974e461
Hugues GUILLEUS <ghugues@netc.fr>
2025-03-25 19:18:40 +0100

    Move service/eu_ec_eci/check.go -> ./fetch_accepted.go

--- service/eu_ec_eci/fetch_accepted.go
+++ service/eu_ec_eci/fetch_accepted.go
@@ -151,6 +151,102 @@ type Document struct {
 	MimeType string
 }
 
+type acceptedDTO struct {
+	Status     string  `json:"status"`
+	LastUpdate dtoTime `json:"latestUpdateDate"`
+	Deadline   dtoDate `json:"deadline"`
+	Logo       struct {
+		ID int `json:"id"`
+	} `json:"logo"`
+	Categories []struct {
+		CategoryType string `json:"categoryType"`
+	} `json:"categories"`
+
+	Description []struct {
+		Original    bool              `json:"Original"`
+		Language    language.Language `json:"languageCode"`
+		Title       string            `json:"title"`
+		SupportLink string            `json:"supportLink"`
+		Website     string            `json:"website"`
+		Objective   string            `json:"objectives"`
+		AnnexDoc    *docDTO           `json:"additionalDocument"`
+		DraftLegal  *docDTO           `json:"draftLegal"`
+		Annex       string            `json:"annexText"`
+		Treaty      string            `json:"treaties"`
+		Register    struct {
+			CELEX       string  `json:"celex"`
+			Corrigendum string  `json:"corrigendum"`
+			Document    *docDTO `json:"document"`
+		} `json:"commissionDecision"`
+	} `json:"linguisticVersions"`
+	PreAnswer struct {
+		Links [1]struct {
+			DefaultLink string `json:"defaultLink"`
+		} `json:"links"`
+	} `json:"preAnswer"`
+
+	Progress []struct {
+		Status string  `json:"Name"`
+		Note   string  `json:"footnoteType"`
+		Date   dtoDate `json:"date"`
+	} `json:"progress"`
+	Answer struct {
+		Links []struct {
+			DefaultLanguageCode language.Language `json:"defaultLanguageCode"`
+			Kind                string            `json:"defaultName"`
+			DefaultLink         string            `json:"defaultLink"`
+			Link                []struct {
+				Language language.Language `json:"languageCode"`
+				Link     string            `json:"link"`
+			} `json:"link"`
+		} `json:"links"`
+	} `json:"answer"`
+
+	SosReport  *signatureDTO `json:"sosReport"`
+	Submission *signatureDTO `json:"submission"`
+
+	Members []struct {
+		FullName         string          `json:"fullName"`
+		Type             string          `json:"type"`
+		URL              string          `json:"email"`
+		ResidenceCountry country.Country `json:"ResidenceCountry"`
+		Start            dtoDate         `json:"startingDate"`
+		Privacy          bool            `json:"privacyApplied"`
+		ReplacedMember   []struct {
+			FullName         string          `json:"fullName"`
+			Type             string          `json:"type"`
+			URL              string          `json:"email"`
+			ResidenceCountry country.Country `json:"residenceCountry"`
+			Start            dtoDate         `json:"startingDate"`
+			End              dtoDate         `json:"endDate"`
+			Privacy          bool            `json:"privacyApplied"`
+		} `json:"replacedMember"`
+	} `json:"members"`
+
+	Funding struct {
+		LastUpdate dtoDate `json:"lastUpdate"`
+		Sponsors   []struct {
+			Amount         float64 `json:"amount"`
+			Date           dtoDate `json:"date"`
+			Name           string  `json:"name"`
+			PrivateSponsor bool    `json:"privateSponsor"`
+			Anonymized     bool    `json:"anonymized"`
+		}
+		TotalAmount float64 `json:"totalAmount"`
+		Document    *docDTO `json:"document"`
+	} `json:"funding"`
+}
+type signatureDTO struct {
+	PaperSignaturesUpdate dtoDate `json:"updateDate"`
+	TotalSignatures       uint    `json:"totalSignatures"`
+
+	Entry []struct {
+		Country country.Country `json:"countryCodeType"`
+		Count   uint            `json:"total"`
+		After   bool            `json:"afterSubmission"`
+	}
+}
+
 func fetchDetail(t *tool.Tool, info indexItem) *ECIOut {
 	request := fetch.URL(fmt.Sprintf(detailURL, info.year, info.number))
 	if tool.DevMode {
